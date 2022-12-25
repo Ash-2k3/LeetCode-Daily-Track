344a8b8df98b4f4eaee78b01f4e7e84094924d00
Ash-2k3 <ash.me.cse@gmail.com>
2022-12-25 20:11:09 +0530

    26 December 2022

---
+++ 25.12.2022/Linked_List_Cycle.cpp
@@ -0,0 +1,38 @@
+/*
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false
+*/
+
+
+// approach 1 
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        unordered_map<ListNode* , bool> m ;
+        ListNode* temp =head ;
+
+        while(temp != NULL){
+            if(m[temp] == true){
+                return true ;
+            }else{
+                m[temp] =true ;
+                temp = temp->next ;
+            }
+        }
+        return false ;
+    }
+};
+
+
